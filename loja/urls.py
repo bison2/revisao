166bee4cb00c329fcaeacc6f8429e4379ff3c2b7
bison2 <profvaldes@gmail.com>
2020-12-21 10:51:45 -0200

    criacao do formulario de contato. Criei o forms.py, suas importacoes, a classe ContactForm. Na views.py fiz o import da forms.py, criei sua funcao contact_page, onde instanciei o contact_form, e o inclui no context. Na urls.py importei essa funcao contact_page e coloquei sua url. No template criei a pasta contact e sua view.html, na qual foi incluso a tag form e o metodo POST or None, o csrf, e o button enviar.

--- loja/urls.py
+++ loja/urls.py
@@ -13,21 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#configuracao do static files
 from django.conf import settings
 from django.conf.urls.static import static
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import home_page, about_page
+#importacao das funcoes da views e inclusao das suas paths
+from .views import home_page, about_page, contact_page
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
+    path('contact/', contact_page, name='contact'),
     path('admin/', admin.site.urls),
 ]
 
 
+
+#outra parte da configuracao do static files
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- loja/views.py
+++ loja/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .forms import ContactForm
 
 def home_page(request):
     context = {
@@ -14,3 +17,15 @@ def about_page(request):
                     "content": "Bem vindo a About Page"
               }
     return render(request, "about/view.html", context)   
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+                    "title": "Contact Page",
+                    "content": "Bem vindo a Contact Page",
+                    "form": contact_form
+                   # "brand": "Novo nome da marca"
+              }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    return render(request, "contact/view.html", context)
